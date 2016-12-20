1cbf67d5a0ebc4940eedb95deaa484368e5d1f8d
DESKTOP-6G543VF\Robin <robin.oscarsson@gmail.com>
2016-12-20 12:39:06 +0100

    Added classes

---
+++ src/AddressBook/CatalogueLoader.java
@@ -0,0 +1,10 @@
+package AddressBook;
+
+/**
+ * Created by Robin on 20/12/2016.
+ */
+public class CatalogueLoader {
+    public void run(){
+
+    }
+}
---
+++ src/AddressBook/RemoteCatalogueFactory.java
@@ -0,0 +1,7 @@
+package AddressBook;
+
+/**
+ * Created by Robin on 20/12/2016.
+ */
+public class RemoteCatalogueFactory {
+}
