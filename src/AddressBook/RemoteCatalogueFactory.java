3035e1f0f3c3f1cacdf560ed0f98ed086da10653
DESKTOP-6G543VF\Robin <robin.oscarsson@gmail.com>
2016-12-20 12:42:34 +0100

    Added RemoteCatalogueFactory

--- src/AddressBook/RemoteCatalogueFactory.java
+++ src/AddressBook/RemoteCatalogueFactory.java
@@ -4,4 +4,9 @@ package AddressBook;
  * Created by Robin on 20/12/2016.
  */
 public class RemoteCatalogueFactory {
+    private int defaultPort;
+
+    RemoteCatalogueFactory (int port){
+        this.defaultPort = port;
+    }
 }
