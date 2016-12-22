e5b09721a4d7908a47353805c0c77cd74d42ae86
RUMPNISSENS-ASK\Gustav <gustav_hassler@hotmail.com>
2016-12-22 13:56:21 +0100

    ContactListSorter fix (for real, this time)

--- src/AddressBook/ContactListSorter.java
+++ src/AddressBook/ContactListSorter.java
@@ -8,13 +8,19 @@ import java.util.List;
 
 public class ContactListSorter {
 
-    public ArrayList<Contact> sort(ArrayList<Contact> contacts){
+    public String sort(ArrayList<Contact> contacts){
+        String sortResults ="";
         Collections.sort(contacts, new Comparator<Contact>() {
             @Override
             public int compare(Contact o1, Contact o2) {
                 return o1.getFirstName().compareTo(o2.getFirstName());
             }
         });
-        return contacts;
+
+        for (Contact contact : contacts) {
+            sortResults += contact.getID() + " " + contact.getFirstName() + " " + contact.getLastName() + " " + contact.getEmailAdress() + "\n";
+        }
+
+        return sortResults;
     }
 }
