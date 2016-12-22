009b9085d680b1247824bb015fbe3101abbfe636
RUMPNISSENS-ASK\Gustav <gustav_hassler@hotmail.com>
2016-12-22 12:06:18 +0100

    Buggfixing #1

--- src/AddressBook/CommandLineInterface.java
+++ src/AddressBook/CommandLineInterface.java
@@ -4,11 +4,13 @@ package AddressBook;
 public class CommandLineInterface implements InputHandler {
 
     CommandInterpreter commandInterpreter = new CommandInterpreter();
+    ConsolePrinter consolePrinter = null;
 
     public CommandLineInterface(){
 
 while (true) {
     Console console = new Console();
+    consolePrinter = console;
     console.registerInputHandler(this);
 }
 
@@ -18,7 +20,7 @@ while (true) {
     public void handle(CommandLine commandLine) {
         //System.out.print("Kommer vi hit?");
         try {
-            commandInterpreter.interpret(commandLine);
+            commandInterpreter.interpret(commandLine, consolePrinter);
         } catch (InvalidCommandParameterException e) {
             e.printStackTrace();
         } catch (InvalidCommandException e) {
