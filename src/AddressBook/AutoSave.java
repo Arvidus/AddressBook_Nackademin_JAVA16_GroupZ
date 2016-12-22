7d2b37a6a544edfc47bcd86513dd4cd020662eae
DESKTOP-6G543VF\Robin <robin.oscarsson@gmail.com>
2016-12-22 15:08:18 +0100

    Fixed AutoSave

--- src/AddressBook/AutoSave.java
+++ src/AddressBook/AutoSave.java
@@ -1,17 +1,21 @@
 package AddressBook;
 
 public class AutoSave {
-    public void save(){
-        new Thread(new Runnable() {
+    public AutoSave() {
+        Thread autoSave = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(5_000);
-                    RegistryPersister.save();
-                } catch(Exception ex) {
-                    ex.printStackTrace();
+                while (true) {
+                    try {
+                        Thread.sleep(5_000);
+                        RegistryPersister.save();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
                 }
             }
         });
+        autoSave.setDaemon(true);
+        autoSave.start();
     }
 }
